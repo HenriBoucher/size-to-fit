e5cc5c9b77e1427e4f8e6069a88848320f09679c
lib121 <lib121@CALSLIB1469.ACDM.DS.SAIT.CA>
2018-08-15 16:34:10 -0600

    First crack at PlatformObject - needs set and get methods

---
+++ workspace/e4fxapp.app/src/myE4Package/DegreeGradientMap.java
@@ -0,0 +1,23 @@
+package myE4Package;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.PlatformObject;
+
+import javafx.animation.Timeline;
+import javafx.scene.paint.LinearGradient;
+
+public class DegreeGradientMap extends PlatformObject {
+	class GradientSet {
+		LinearGradient gradient;
+		Timeline forward;
+		Timeline reverse;
+	}
+	
+	private Map <Double, GradientSet> dg = new HashMap <Double, GradientSet>();
+	
+	DegreeGradientMap() {
+	}
+	
+}
