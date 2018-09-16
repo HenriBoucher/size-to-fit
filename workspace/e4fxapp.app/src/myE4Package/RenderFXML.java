ae0b163cbcbe28a2a4b8ab47e1d503e51b08d35b
tfdllc137 <tfdllc137@TFDL-pc10103.uc.ucalgary.ca>
2018-09-16 16:09:23 -0600

    BoderPane to GridPane   and   8.0.171 to 8.0.161
    It pays to review a stack trace.  Half way down it complained about not
    being able to convert a GridPane to a BorderPane.
    
    The root element has xmlns="http://javafx.com/javafx/8.0.161" which
    produces a warning.  It still works.

--- workspace/e4fxapp.app/src/myE4Package/RenderFXML.java
+++ workspace/e4fxapp.app/src/myE4Package/RenderFXML.java
@@ -2,21 +2,20 @@ package myE4Package;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class RenderFXML extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		BorderPane root = null;
+		GridPane root = null;
 		try {
-			root = (BorderPane) FXMLLoader.load(getClass().getResource("test.fxml"));
+			root = (GridPane) FXMLLoader.load(getClass().getResource("test.fxml"));
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -28,7 +27,6 @@ public class RenderFXML extends Application {
 		Scene scene = new Scene(root, 600, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
 	}
 
 	public static void main(String[] args) {
