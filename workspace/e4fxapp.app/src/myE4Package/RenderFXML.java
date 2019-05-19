d9fba7727123954b0981a3ce4601d201ad48ac8e
tfdllc201 <tfdllc201@TFDL-PC10129.uc.ucalgary.ca>
2019-05-19 16:58:45 -0600

    Need to prefix scene.lookup() with "#" and Bounds
    Unfortunately Bounds don't have properties for minX.

--- workspace/e4fxapp.app/src/myE4Package/RenderFXML.java
+++ workspace/e4fxapp.app/src/myE4Package/RenderFXML.java
@@ -1,5 +1,6 @@
 package myE4Package;
 
+import java.awt.Rectangle;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -9,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.geometry.Bounds;
 
 public class RenderFXML extends Application {
 
@@ -24,33 +26,57 @@ public class RenderFXML extends Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		Scene scene = new Scene(root, 600, 600);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 		final GridPane g = root;
 		g.setGridLinesVisible(true);
-		root.getChildren().forEach(child -> {
+		g.getChildren().forEach(child -> {
+			if (child instanceof Line) {
+				System.out.println("css id = " + child.getId());
+			}
 			if (child instanceof Pipe) {
 				Pipe p = (Pipe) child;
 				System.out.println("parent = " + p.getParent().getId());
 				System.err.println("in RenderFXML " + p.getId());
-				String headId = "-pipe-" + p.getHeadColumn() + "-" + p.getHeadHPos() + "-" + p.getHeadRow() + "-" + p.getHeadVPos();
-				Line head = (Line) g.lookup(headId);
+				String headId = "#-pipe-" + p.getHeadColumn() + "-" + p.getHeadHPos() + "-" + p.getHeadRow() + "-" + p.getHeadVPos();
+				Line head = (Line) scene.lookup(headId);
+				System.out.println(head);
 				if (head == null) {
 					Line h = new Line();
 					GridPane.setConstraints(h, p.getHeadColumn(), p.getHeadRow(), 1, 1, p.getHeadHPos(), p.getHeadVPos());
+					h.setStrokeWidth(10);
+					Rectangle rect = new Rectangle();
+					
 					h.setId(headId);
 					head = h;
 				}
-				head.startXProperty().bind(p.xProperty());
-//				p.xProperty().bind(head.startXProperty());  // you can't do both
-				String tailId = "-pipe-" + p.getTailColumn() + "-" + p.getTailHPos() + "-" + p.getTailRow() + "-" + p.getTailVPos();
+//				head.startXProperty().bind(p.xProperty());
+				System.out.println(head);
+				p.xProperty().bind(head.startXProperty());  // you can't do both
+				String tailId = "#-pipe-" + p.getTailColumn() + "-" + p.getTailHPos() + "-" + p.getTailRow() + "-" + p.getTailVPos();
+				Line tail = (Line) scene.lookup(tailId);
+//				p.widthProperty().bind(tail.layoutXProperty().subtract(head.layoutXProperty()));
+				
+				Bounds boundsInSceneTail = tail.localToScene(tail.getBoundsInLocal());
+				Bounds boundsInSceneHead = head.localToScene(head.getBoundsInLocal());
+				System.out.println("tail = " + boundsInSceneTail);
+				System.out.println("head = " + boundsInSceneHead);
+				p.setWidth(boundsInSceneHead.getMinX() - boundsInSceneTail.getMinX());
+//				p.widthProperty().bind(boundsInSceneHead.subtract(head.layoutXProperty()));
+
 				System.out.println(headId);
+				System.out.println(tailId);
 			}
 		});
 		
 		
-		Scene scene = new Scene(root, 600, 600);
+//		Scene scene = new Scene(root, 600, 600);
 		
-		primaryStage.setScene(scene);
-		primaryStage.show();
+//		primaryStage.setScene(scene);
+//		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
