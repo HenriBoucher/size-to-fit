b1fb7c35015ee217703a0304255d9552b7929de8
tfdllc148 <tfdllc148@TFDL-PC10129.uc.ucalgary.ca>
2019-06-02 16:00:24 -0600

    New Pane program
    To implement bindings on panes.

---
+++ workspace/e4fxapp.app/src/myE4Package/Pane.java
@@ -0,0 +1,43 @@
+package myE4Package;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import javafx.geometry.Bounds;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.ObjectBinding;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import myE4Package.Pipe;
+import javafx.scene.shape.Rectangle;
+
+
+public class Pane extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) {
+	GridPane root = null;
+	try {
+		root = (GridPane) FXMLLoader.load(getClass().getResource("test.fxml"));
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	Scene scene = new Scene(root, 600, 600);
+	primaryStage.setScene(scene);
+	primaryStage.show();
+	
+	}
+
+}
